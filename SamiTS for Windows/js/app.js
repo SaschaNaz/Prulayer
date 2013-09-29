a1d918a9054383adddc6ff715ae9d73a3d1207df
SaschaNaz <saschanaz@outlook.com>
2013-09-30 02:14:37 +0900

    UI update

--- SamiTS for Windows/js/app.js
+++ SamiTS for Windows/js/app.js
@@ -2,10 +2,13 @@
 ///<reference path="winrt.d.ts" />
 ///<reference path="SamiTS/SamiTS/samiconverter.ts" />
 "use strict";
+//declare var output: HTMLTextAreaElement;
 var track;
 var style;
-var isPreviewAreaShown = false;
+
+//var isPreviewAreaShown = false;
 var subtitleFileDisplayName;
+var cursorTimerId;
 
 var SubType;
 (function (SubType) {
@@ -13,7 +16,44 @@ var SubType;
     SubType[SubType["SRT"] = 1] = "SRT";
 })(SubType || (SubType = {}));
 
+WinJS.UI["eventHandler"](play);
+WinJS.UI["eventHandler"](pause);
+WinJS.Namespace.define("startPage", {
+    playHandler: play,
+    pauseHandler: pause
+});
+
+function play(evt) {
+    if (cursorTimerId)
+        clearTimeout(cursorTimerId);
+    cursorTimerId = setTimeout(function () {
+        mediaplayer.style.cursor = "none";
+    }, 3000);
+}
+function pause(evt) {
+    if (cursorTimerId)
+        clearTimeout(cursorTimerId);
+    mediaplayer.style.cursor = "auto";
+}
+function pointermove(evt) {
+    if (cursorTimerId)
+        clearTimeout(cursorTimerId);
+    mediaplayer.style.cursor = "auto";
+    if (!mediaplayer.winControl.paused)
+        cursorTimerId = setTimeout(function () {
+            mediaplayer.style.cursor = "none";
+        }, 3000);
+}
+function pointerdown(evt) {
+    if ((mediaplayer.winControl.readyState == 4) && (evt.pointerType !== "mouse" || evt.button != 2))
+        if (mediaplayer.winControl.paused)
+            mediaplayer.winControl.play();
+else
+            mediaplayer.winControl.pause();
+}
+
 function load(evt) {
+    var player = mediaplayer.getElementsByTagName("video")[0];
     var files = (evt.target).files;
     var videofile;
     var subfile;
@@ -31,17 +71,16 @@ else if (!subfile && getFileExtension(file) === "smi")
 
     subtitleFileDisplayName = getFileDisplayName(subfile);
     var resultOutput = function (result) {
-        hidePreviewArea();
-        hidePlayer();
-        hideAreaSelector();
-        output.value = result;
         if (track) {
             player.removeChild(track);
             (document.getElementById("areaselector"));
-            player.src = '';
+            mediaplayer.winControl.src = '';
+            //player.src = '';
         }
         if (videofile) {
-            player.src = URL.createObjectURL(videofile);
+            mediaplayer.winControl.src = URL.createObjectURL(videofile);
+
+            //player.src = URL.createObjectURL(videofile);
             track = document.createElement("track");
             track.label = "日本語";
             track.kind = "subtitles";
@@ -49,10 +88,12 @@ else if (!subfile && getFileExtension(file) === "smi")
             track.src = URL.createObjectURL(new Blob([result], { type: "text/vtt" }));
             track.default = true;
             player.appendChild(track);
-            showAreaSelector();
-            showPlayer();
-        } else
-            showPreviewArea();
+            mediaplayer.winControl.play();
+            //showAreaSelector();
+            //showPlayer();
+        }
+        //else
+        //    showPreviewArea();
     };
 
     return SamiTS.convertToWebVTTFromFile(subfile, resultOutput, function (resultStyle) {
@@ -87,39 +128,38 @@ else if (!subfile && getFileExtension(file) === "smi")
     //});
 }
 
-function selectArea() {
-    if (isPreviewAreaShown) {
-        hidePreviewArea();
-        showPlayer();
-    } else {
-        hidePlayer();
-        showPreviewArea();
-    }
-}
-
-function showAreaSelector() {
-    areaselector.style.display = "inline-block";
-}
-function hideAreaSelector() {
-    areaselector.style.display = "none";
-}
-function showPreviewArea() {
-    previewarea.style.display = "inline-block";
-    isPreviewAreaShown = true;
-    areaselector.value = "Play";
-}
-function hidePreviewArea() {
-    previewarea.style.display = "none";
-    isPreviewAreaShown = false;
-}
-function showPlayer() {
-    player.style.display = "inline-block";
-    areaselector.value = "Preview";
-}
-function hidePlayer() {
-    player.style.display = "none";
-}
-
+//function selectArea() {
+//    if (isPreviewAreaShown) {
+//        hidePreviewArea();
+//        showPlayer();
+//    }
+//    else {
+//        hidePlayer();
+//        showPreviewArea();
+//    }
+//}
+//function showAreaSelector() {
+//    areaselector.style.display = "inline-block";
+//}
+//function hideAreaSelector() {
+//    areaselector.style.display = "none";
+//}
+//function showPreviewArea() {
+//    previewarea.style.display = "inline-block";
+//    isPreviewAreaShown = true;
+//    areaselector.value = "Play";
+//}
+//function hidePreviewArea() {
+//    previewarea.style.display = "none";
+//    isPreviewAreaShown = false;
+//}
+//function showPlayer() {
+//    //player.style.display = "inline-block";
+//    areaselector.value = "Preview";
+//}
+//function hidePlayer() {
+//    //player.style.display = "none";
+//}
 function getExtensionForSubType(subtype) {
     switch (subtype) {
         case SubType.WebVTT:
@@ -148,3 +188,4 @@ function getFileDisplayName(file) {
     splitted = splitted.slice(0, splitted.length - 1);
     return splitted.join('.');
 }
+//# sourceMappingURL=app.js.map
--- SamiTS for Windows/js/app.ts
+++ SamiTS for Windows/js/app.ts
@@ -3,26 +3,66 @@
 ///<reference path="SamiTS/SamiTS/samiconverter.ts" />
 "use strict";
 
-declare var areaselector: HTMLButtonElement;
-declare var previewarea: HTMLDivElement;
-declare var player: HTMLVideoElement;
-declare var output: HTMLTextAreaElement;
+//declare var areaselector: HTMLButtonElement;
+//declare var previewarea: HTMLDivElement;
+declare var mediaplayer: HTMLDivElement;
+//declare var player: HTMLVideoElement;
+declare var appBar: any;
+//declare var output: HTMLTextAreaElement;
 var track: HTMLTrackElement;
 var style: HTMLStyleElement;
-var isPreviewAreaShown = false;
+//var isPreviewAreaShown = false;
 var subtitleFileDisplayName: string;
+var cursorTimerId: number;
 
 enum SubType {
     WebVTT, SRT
 }
 
+WinJS.UI["eventHandler"](play);
+WinJS.UI["eventHandler"](pause);
+WinJS.Namespace.define("startPage", {
+    playHandler: play,
+    pauseHandler: pause
+});
+
+function play(evt: Event) {
+    if (cursorTimerId)
+        clearTimeout(cursorTimerId);
+    cursorTimerId = setTimeout(() => {
+        mediaplayer.style.cursor = "none";
+    }, 3000);
+}
+function pause(evt: Event) {
+    if (cursorTimerId)
+        clearTimeout(cursorTimerId);
+    mediaplayer.style.cursor = "auto";
+}
+function pointermove(evt: Event) {
+    if (cursorTimerId)
+        clearTimeout(cursorTimerId);
+    mediaplayer.style.cursor = "auto";
+    if (!mediaplayer.winControl.paused)
+        cursorTimerId = setTimeout(() => {
+            mediaplayer.style.cursor = "none";
+        }, 3000);
+}
+function pointerdown(evt: MSPointerEvent) {
+    if ((mediaplayer.winControl.readyState == 4) && (evt.pointerType !== "mouse" || evt.button != 2))
+        if (mediaplayer.winControl.paused)
+            mediaplayer.winControl.play();
+        else
+            mediaplayer.winControl.pause();
+}
+
 function load(evt: Event) {
+    var player: HTMLVideoElement = mediaplayer.getElementsByTagName("video")[0];
     var files = (<HTMLInputElement>evt.target).files;
     var videofile: File;
     var subfile: File;
     for (var i = 0; i < files.length; i++) {
         var file = files[i];
-        if (!videofile && player.canPlayType(file.type))
+        if (!videofile && player.canPlayType(file.type)) //mediaplayer.winControl.canPlayType(file.type))
             videofile = file;
         else if (!subfile && getFileExtension(file) === "smi")
             subfile = file;
@@ -34,16 +74,18 @@ function load(evt: Event) {
 
     subtitleFileDisplayName = getFileDisplayName(subfile);
     var resultOutput = (result: string) => {
-        hidePreviewArea();
-        hidePlayer();
-        hideAreaSelector();
-        output.value = result;
+        //hidePreviewArea();
+        //hidePlayer();
+        //hideAreaSelector();
+        //output.value = result;
         if (track) {
             player.removeChild(track); (<HTMLButtonElement>document.getElementById("areaselector"))
-            player.src = '';
+            mediaplayer.winControl.src = '';
+            //player.src = '';
         }
         if (videofile) {
-            player.src = URL.createObjectURL(videofile);
+            mediaplayer.winControl.src = URL.createObjectURL(videofile);
+            //player.src = URL.createObjectURL(videofile);
             track = document.createElement("track");
             track.label = "日本語";
             track.kind = "subtitles";
@@ -51,11 +93,12 @@ function load(evt: Event) {
             track.src = URL.createObjectURL(new Blob([result], { type: "text/vtt" }));
             track.default = true;
             player.appendChild(track);
-            showAreaSelector();
-            showPlayer();
+            mediaplayer.winControl.play();
+            //showAreaSelector();
+            //showPlayer();
         }
-        else
-            showPreviewArea();
+        //else
+        //    showPreviewArea();
     };
 
     return SamiTS.convertToWebVTTFromFile(subfile, resultOutput,
@@ -92,39 +135,39 @@ function load(evt: Event) {
     //});
 }
 
-function selectArea() {
-    if (isPreviewAreaShown) {
-        hidePreviewArea();
-        showPlayer();
-    }
-    else {
-        hidePlayer();
-        showPreviewArea();
-    }
-}
+//function selectArea() {
+//    if (isPreviewAreaShown) {
+//        hidePreviewArea();
+//        showPlayer();
+//    }
+//    else {
+//        hidePlayer();
+//        showPreviewArea();
+//    }
+//}
 
-function showAreaSelector() {
-    areaselector.style.display = "inline-block";
-}
-function hideAreaSelector() {
-    areaselector.style.display = "none";
-}
-function showPreviewArea() {
-    previewarea.style.display = "inline-block";
-    isPreviewAreaShown = true;
-    areaselector.value = "Play";
-}
-function hidePreviewArea() {
-    previewarea.style.display = "none";
-    isPreviewAreaShown = false;
-}
-function showPlayer() {
-    player.style.display = "inline-block";
-    areaselector.value = "Preview";
-}
-function hidePlayer() {
-    player.style.display = "none";
-}
+//function showAreaSelector() {
+//    areaselector.style.display = "inline-block";
+//}
+//function hideAreaSelector() {
+//    areaselector.style.display = "none";
+//}
+//function showPreviewArea() {
+//    previewarea.style.display = "inline-block";
+//    isPreviewAreaShown = true;
+//    areaselector.value = "Play";
+//}
+//function hidePreviewArea() {
+//    previewarea.style.display = "none";
+//    isPreviewAreaShown = false;
+//}
+//function showPlayer() {
+//    //player.style.display = "inline-block";
+//    areaselector.value = "Preview";
+//}
+//function hidePlayer() {
+//    //player.style.display = "none";
+//}
 
 function getExtensionForSubType(subtype: SubType) {
     switch (subtype) {
